77618519c943bd7fbd903e5306ac517eb0316aa2
rona-tombo <ronatombo@gmail.com>
2021-11-19 07:58:47 +0900

    🎉 Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "land.vani"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
