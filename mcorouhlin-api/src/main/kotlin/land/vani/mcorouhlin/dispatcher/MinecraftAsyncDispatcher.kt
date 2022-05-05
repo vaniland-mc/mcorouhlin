422fe842a06964610d9f1067d02059ee36036808
rona-tombo <ronatombo@gmail.com>
2022-05-05 17:15:00 +0900

    💚 Fix detekt CI

--- mcorouhlin-api/src/main/kotlin/land/vani/mcorouhlin/dispatcher/MinecraftAsyncDispatcher.kt
+++ mcorouhlin-api/src/main/kotlin/land/vani/mcorouhlin/dispatcher/MinecraftAsyncDispatcher.kt
@@ -3,5 +3,6 @@ package land.vani.mcorouhlin.dispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import org.jetbrains.annotations.NonBlockingExecutor
 
+@Suppress("UnnecessaryAbstractClass")
 @NonBlockingExecutor
 abstract class MinecraftAsyncDispatcher : CoroutineDispatcher()
